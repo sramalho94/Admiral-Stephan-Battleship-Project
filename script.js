ea268bc41e7b3881017007bdfbd875d951a040a4
Stephan D Ramalho <sramalho@fordham.edu>
2022-10-03 11:08:37 -0400

    enemy grid cells now have unique ID's that reflect their positions

--- script.js
+++ script.js
@@ -35,8 +35,30 @@ const setEnemyBoard = () => {
   for (let i = 0; i < 100; i++) {
     const cellEnemy = document.createElement('div')
     cellEnemy.classList.add('cellEnemy')
-    cellEnemy.setAttribute('id', `cellEnemy${i}`)
     gridEnemy.append(cellEnemy)
+    if (i <= 9) {
+      cellEnemy.setAttribute('id', `A${i}` + 'enemy')
+    } else if (i >= 10 && i <= 19) {
+      for (let b = 1; b <= 10; b++) {
+        cellEnemy.setAttribute('id', 'B' + (i - b) + 'enemy')
+      }
+    } else if (i >= 20 && i <= 29) {
+      cellEnemy.setAttribute('id', 'C' + (i - 20) + 'enemy')
+    } else if (i >= 30 && i <= 39) {
+      cellEnemy.setAttribute('id', 'D' + (i - 30) + 'enemy')
+    } else if (i >= 40 && i <= 49) {
+      cellEnemy.setAttribute('id', 'E' + (i - 40) + 'enemy')
+    } else if (i >= 50 && i <= 59) {
+      cellEnemy.setAttribute('id', 'F' + (i - 50) + 'enemy')
+    } else if (i >= 60 && i <= 69) {
+      cellEnemy.setAttribute('id', 'G' + (i - 60) + 'enemy')
+    } else if (i >= 70 && i <= 79) {
+      cellEnemy.setAttribute('id', 'H' + (i - 70) + 'enemy')
+    } else if (i >= 80 && i <= 89) {
+      cellEnemy.setAttribute('id', 'I' + (i - 80) + 'enemy')
+    } else if (i >= 90 && i <= 99) {
+      cellEnemy.setAttribute('id', 'J' + (i - 90) + 'enemy')
+    }
   }
 }
 setPlayerBoard()
